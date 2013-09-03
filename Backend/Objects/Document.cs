2013c5831362fbe521074af1edc77a38e267f71a
taivasverkko <heikki.hyttinen83@gmail.com>
2013-09-03 20:52:01 +0300

    Päivitelty DtcCodeObject ja lisätty Document

---
+++ Backend/Objects/Document.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Backend.Objects
+{
+    /// <summary>
+    /// Tarkoitettu Documenteille mitä listänään DtcCodeObjekteihin
+    /// </summary>
+    public class Document
+    {
+        string Name { get; set; }
+        string Description { get; set; }
+        string Filename { get; set; }
+    }
+}
--- Backend/Objects/DtcCodeObject.cs
+++ Backend/Objects/DtcCodeObject.cs
@@ -1,17 +1,47 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Collections.ObjectModel;
 
 namespace Backend.Objects
 {
+    /// <summary>
+    /// DtcCodeObject on se objekti mikä sisältää kaikki DTC tiedot
+    /// </summary>
     public class DtcCodeObject
     {
+        /// <summary>
+        /// Valmistaja
+        /// </summary>
         public string Manufacturer { get; set; }
+
+        /// <summary>
+        /// Malli
+        /// </summary>
         public string Model { get; set; }
+
+        /// <summary>
+        /// Moottori
+        /// </summary>
         public string Engine { get; set; }
+        
+        /// <summary>
+        /// DTC
+        /// </summary>
         public string DTC { get; set; }
+        
+        /// <summary>
+        /// Kuvaus
+        /// </summary>
+        public string Description { get; set; }
+        
+        /// <summary>
+        ///  Vuosimalli
+        /// </summary>
         public int Year { get; set; }
+  
+        /// <summary>
+        /// Documentit / Tiedostot. JPG, PDF, DOCX.
+        /// </summary>
+        ObservableCollection<Document> Documents { get; set; }
+
     }
 }
