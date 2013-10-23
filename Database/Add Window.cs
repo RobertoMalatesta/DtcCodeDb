e046260a49f85f993640807739a333a75528fc10
Kleptomaani <markods@hotmail.com>
2013-10-23 19:59:41 +0300

    Revert "Import toiminto"
    This reverts commit a7c5aa67f201725247813b49f0849efdb61ce145.

--- Database/Add Window.cs
+++ Database/Add Window.cs
@@ -55,50 +55,5 @@ namespace Database
         {
 
         }
-
-        private void contextMenuStrip1_Opening(object sender, CancelEventArgs e)
-        {
-
-        }
-
-        private void Importbutton_Click(object sender, EventArgs e)// Tästä alas omaa settiä
-        {
-            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
-            saveFileDialog1.Filter = "JPeg Image|*.jpg|Bitmap Image|*.bmp|Gif Image|*.gif";
-            saveFileDialog1.Title = "Tallenna kuva tiedosto";
-            saveFileDialog1.ShowDialog();
-
-            // If the file name is not an empty string open it for saving.
-              if(saveFileDialog1.FileName != "")
-            {
-              // Saves the Image via a FileStream created by the OpenFile method.
-              System.IO.FileStream fs = 
-                 (System.IO.FileStream)saveFileDialog1.OpenFile();
-              // Saves the Image in the appropriate ImageFormat based upon the
-              // File type selected in the dialog box.
-              // NOTE that the FilterIndex property is one-based.
-              switch(saveFileDialog1.FilterIndex)
-                      {
-                         case 1 : 
-                         this.Importbutton.Image.Save(fs, 
-                            System.Drawing.Imaging.ImageFormat.Jpeg);
-                         break;
-
-                         case 2 : 
-                         this.Importbutton.Image.Save(fs, 
-                            System.Drawing.Imaging.ImageFormat.Bmp);
-                         break;
-
-                         case 3 : 
-                         this.Importbutton.Image.Save(fs, 
-                            System.Drawing.Imaging.ImageFormat.Gif);
-                         break;
-                     }
-
-           fs.Close();
-            }
-        }
-
-        }
     }
-
+}
